aac95750fe99fde8ee1af03677bc99a5e66eb4f7
muyi2905 <obaremimuyiwa@gmail.com>
2023-09-01 12:07:32 +0000

    decrypt

--- main.py
+++ main.py
@@ -19,3 +19,11 @@ def encrypt(plain_text, shift_amount):
   print(f"The encoded text is {cipher_text}")
 
 encrypt(plain_text=text, shift_amount=shift)
+
+def decrypt(cipher_text, shift_amount):
+      plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"The decoded text is {plain_text}")
