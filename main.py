83dd4eb1d02a784074ceea790c3aea66189cb0b3
muyi2905 <obaremimuyiwa@gmail.com>
2023-09-01 11:55:01 +0000

    fixed inded out of range error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+            "n","o","p","q","r","s","t","u","v","w","x","y","z"
+            "a","b","c","d","e","f","g","h","i","j","k","l","m",
             "n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 direction =input("Type 'encode' to encrypt, type 'decode' to decrypt :\n")
