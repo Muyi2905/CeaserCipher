b79b3c5a61c5106559835966a4f26a8f8fc8383f
muyi2905 <obaremimuyiwa@gmail.com>
2023-09-01 11:44:12 +0000

    encrypt function done

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
 
 direction =input("Type 'encode' to encrypt, type 'decode' to decrypt :\n")
 
-text =input("Tyoe your message :\n").lower()
+text =input("Type your message :\n").lower()
 shift =int(input("Type the shift number:\n"))
 
-def encrypt ():
-    text=
-    shift=
+def encrypt (plain_text, shift_amount):
+    cipher_text = ""
+for letter in plain_text:
+  position = alphabet.index(letter)
+  new_position = position + shift_amount
+  new_letter = alphabet[new_position]
+  cipher_text += new_letter
+print(f"the encoded text is {cipher_text}")
+
+encrypt(plain_text=text, shift_amount=shift)
+
+  
+ 
