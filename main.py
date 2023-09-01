f413eec43dce5b70614179c55dbd627bb6b25232
muyi2905 <obaremimuyiwa@gmail.com>
2023-09-01 11:23:51 +0000

    commit

---
+++ main.py
@@ -0,0 +1,8 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+            "n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+direction =input("Type 'encode' to encrypt, type 'decode' to decrypt :\n")
+
+text =input("Tyoe your message :\n").lower()
+shift =int(input("Type the shift number:\n"))
+
